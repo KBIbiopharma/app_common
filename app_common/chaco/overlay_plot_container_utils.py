a4469d329e9e1474b4b19f44b8b074e0c7d44d76
Jonathan Rocher <jrocher@kbibiopharma.com>
2020-02-12 10:59:55 -0700

    Finish docstring.

--- app_common/chaco/overlay_plot_container_utils.py
+++ app_common/chaco/overlay_plot_container_utils.py
@@ -13,7 +13,21 @@ def align_renderer(renderer, axis, dim="index"):
     mapper. Obviously, making the 2 mappers the same object works well to align
     multiple plots with each other.
 
-    But in some situations, having the same mappers leads to
+    But having the same mappers leads to the wrong behavior of the ZoomTool's
+    box drawing feature. The only way to make it work is for mappers to remain
+    distinct but aligned. This function does that.
+
+    Parameters
+    ----------
+    renderer : AbstractPlotRenderer
+        Renderer to align.
+
+    axis : PlotAxis
+        Axis to align the renderer along.
+
+    dim : str
+        Dimension along which to align the renderer. Valid values are "index"
+        and "value".
     """
     axis.mapper.range.add(getattr(renderer, dim))
     mapper = getattr(renderer, dim + "_mapper")
