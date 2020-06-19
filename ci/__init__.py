7c7928836dfc23723cd0d74ccd6ceab9618cd098
Jonathan Rocher <jrocher@kbibiopharma.com>
2020-06-18 22:20:34 -0500

    Empty init file.

--- ci/__init__.py
+++
@@ -1,7 +0,0 @@
-# Enthought product code
-#
-# (C) Copyright 2018-2019 Enthought, Inc., Austin, TX
-# All rights reserved.
-#
-# This file and its contents are confidential information and NOT open source.
-# Distribution is prohibited.
