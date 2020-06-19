2e5003f261ecd27bf212502f3554b0e50151a660
Jonathan Rocher <jrocher@kbibiopharma.com>
2020-06-18 22:19:19 -0500

    Add requirement list.

---
+++ ci/__init__.py
@@ -0,0 +1,7 @@
+# Enthought product code
+#
+# (C) Copyright 2018-2019 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This file and its contents are confidential information and NOT open source.
+# Distribution is prohibited.
