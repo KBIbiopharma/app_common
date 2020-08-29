53d4f536f6109b0b42a7d25393bcdcee59c38eab
Jonathan Rocher <jrocher@kbibiopharma.com>
2020-08-28 22:23:55 -0500

    Add smoke test.

---
+++ app_common/traitsui/tests/test_label_with_html.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from traits.api import HasTraits
+from traitsui.api import View
+
+from app_common.traitsui.label_with_html import Label
+from app_common.apptools.testing_utils import assert_obj_gui_works
+
+
+class A(HasTraits):
+    view = View(
+        Label("blah"),
+        Label("blah", color="red", bold=True),
+        Label("blah", color="#f133a0", bold=True, italic=True),
+        Label("blah", color="tomato", font_family="verdana",
+              font_size=48),
+        Label("blah", bg_color="powderblue"),
+        resizable=True, width=500
+    )
+
+
+class TestLabel(TestCase):
+    def test_ui(self):
+        assert_obj_gui_works(A())
