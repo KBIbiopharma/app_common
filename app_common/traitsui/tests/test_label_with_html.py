bc566377c3822da44a53a73ba7745034100cd937
Jonathan Rocher <jrocher@kbibiopharma.com>
2020-08-28 22:36:52 -0500

    Improve test for new Label functionality.

--- app_common/traitsui/tests/test_label_with_html.py
+++ app_common/traitsui/tests/test_label_with_html.py
@@ -3,7 +3,7 @@ from traits.api import HasTraits
 from traitsui.api import View
 
 from app_common.traitsui.label_with_html import Label
-from app_common.apptools.testing_utils import assert_obj_gui_works
+from app_common.apptools.testing_utils import assert_obj_gui_works, temp_bringup_ui_for
 
 
 class A(HasTraits):
@@ -19,4 +19,22 @@ class A(HasTraits):
 
 class TestLabel(TestCase):
     def test_ui(self):
-        assert_obj_gui_works(A())
+        with temp_bringup_ui_for(A()) as ui:
+            main_group = ui.view_elements.content["view"].content.content[0]
+            item = main_group.content[0]
+            self.assertEqual(item.label, '<p style=color:black>blah</p>')
+
+            item = main_group.content[1]
+            self.assertEqual(item.label, '<b><p style=color:red>blah</p></b>')
+
+            item = main_group.content[2]
+            self.assertEqual(item.label,
+                             '<i><b><p style=color:#f133a0>blah</p></b></i>')
+
+            item = main_group.content[3]
+            self.assertEqual(item.label,
+                             '<p style=color:tomato;font-family:verdana;font-size:48px>blah</p>')  # noqa
+
+            item = main_group.content[4]
+            self.assertEqual(item.label,
+                             '<p style=color:black;background-color:powderblue>blah</p>')  # noqa
