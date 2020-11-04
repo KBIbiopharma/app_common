c9e446a7f2c98a40dc4585fc7bd9f6e537c48e46
Jonathan Rocher <jrocher@kbibiopharma.com>
2020-11-04 17:14:31 -0600

    Add more pandas assertions.

--- app_common/apptools/assertion_utils.py
+++ app_common/apptools/assertion_utils.py
@@ -1,8 +1,32 @@
 from nose.tools import assert_equal
+from pandas.testing import assert_frame_equal, assert_series_equal
 
 from scimath.units.api import UnitArray, UnitScalar
 from app_common.scimath.assertion_utils import assert_unit_array_almost_equal,\
     assert_unit_scalar_almost_equal
+
+def assert_frame_not_equal(*args, **kwargs):
+    """ Not implemented in pandas."""
+    try:
+        assert_frame_equal(*args, **kwargs)
+    except AssertionError:
+        # frames are not equal
+        pass
+    else:
+        # frames are equal
+        raise AssertionError
+
+
+def assert_series_not_equal(*args, **kwargs):
+    """ Not implemented in pandas."""
+    try:
+        assert_series_equal(*args, **kwargs)
+    except AssertionError:
+        # Series are not equal
+        pass
+    else:
+        # Series are equal
+        raise AssertionError
 
 
 def flexible_assert_equal(x1, x2, **kw):
